72c0ceeb5640d46bdec8c1548341ab297830676a
abdullahsami77 <120662449+abdullahsami77@users.noreply.github.com>
2024-12-13 20:54:35 +0200

    Update Book.fs (abdullah)

--- FSharpLibrary/Book.fs
+++ FSharpLibrary/Book.fs
@@ -6,6 +6,8 @@ type Book(title: string, author: string, genre: string) =
     member val Title = title with get, set
     member val Author = author with get, set
     member val Genre = genre with get, set
+    member val IsBorrowed = false with get, set
+    member val BorrowDate: DateTime option = None with get, set
 
      // Method to borrow a book
     member this.Borrow() =
